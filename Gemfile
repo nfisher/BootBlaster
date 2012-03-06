4f1ecc4b551c1fd716222deb3c7e3f993b3a3b81
Nathan Fisher <nfisher@junctionbox.ca>
2012-03-06 21:20:04 +1100

    Rename test cases to work with auto test from ZenTest gem.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 group :test do
 	gem 'mocha', '0.10.3'
+	gem 'ZenTest', '4.6.2' 
 end
--- test/test_boot_blaster.rb
+++ test/test_boot_blaster.rb
@@ -5,8 +5,4 @@ class BootBlasterTest < Test::Unit::TestCase
 	def test_answer_to_everything
 		assert_equal 42, 42
 	end
-
-	def test_query_virtualbox_for_available_machines
-		assert_equal true, true
-	end
 end
