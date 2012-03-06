efa1e1976022b30986d98368651317869a11846c
Nathan Fisher <nfisher@junctionbox.ca>
2012-03-07 00:04:49 +1100

    Move test cases to unit sub-directory.

--- test/unit/test_boot_blaster.rb
+++ test/unit/test_boot_blaster.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require 'mocha'
 
-class BootBlasterTest < Test::Unit::TestCase
+class TestBootBlaster < Test::Unit::TestCase
 	def test_answer_to_everything
 		assert_equal 42, 42
 	end
